613815489120840871365647083b81859d47d547
Javocz <javocz.10@gmail.com>
2018-08-14 21:49:29 -0300

    ejercicio en clase

---
+++ Clase 01/Alumno/Ejercicio/script.js
@@ -0,0 +1,44 @@
+// NOMBRE
+
+// var nombre = prompt ('Ingresá tu nombre.')
+
+// console.log('Bienvenido ' + nombre + ', en qué puedo ayudarte?')
+
+
+
+
+// SUMA
+
+// var inicial = 17
+// var suma = prompt('Ingresá un número.')
+// var parsedSuma = parseFloat(suma, 10)
+
+// var resultado = parsedSuma + inicial
+
+// console.log (parsedSuma + ' + ' + inicial + ' = ' + resultado)
+
+
+// RESTA
+
+// var fecha = 2018
+// var resta = prompt('En qué año naciste?')
+// var parsedResta = parseFloat(resta, 10)
+
+// var edad = fecha - parsedResta
+
+// console.log ('Tenes ' + edad + ' años.')
+
+
+
+
+// DOBLE
+
+var numero1 = prompt('Ingresa un número.')
+var numero2 = prompt('Ingresa otro número.')
+
+var parsedNumero1 = parseFloat(numero1, 10)
+var parsedNumero2 = parseFloat(numero2, 10)
+
+var doble = parsedNumero1 + parsedNumero2
+
+console.log (parsedNumero1 + ' más ' + parsedNumero2 + ' suman ' + doble )
