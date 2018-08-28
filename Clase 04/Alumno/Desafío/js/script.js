38b50a1cb83c442f0d804b4fa399eeb96a4fd064
Javocz <Javocz.10@gmail.com>
2018-08-28 18:41:42 -0300

    Desafio clase 4

--- Clase 04/Alumno/Desafío/js/script.js
+++ Clase 04/Alumno/Desafío/js/script.js
@@ -20,3 +20,36 @@ var studentsList = [
 		dni: 45678983
 	}
 ];
+
+// var busqueda = prompt('A quién buscas?');
+
+// for (i = 0; i < studentsList.length; i++) {
+// 	nombre = studentsList[i].firstName;
+// 	apellido = studentsList[i].lastName;
+
+// 	if (busqueda === nombre || busqueda === apellido) {
+// 		console.log('El estudiante esta el la posición ' + i);
+// 	} else {
+// 		console.log('No existe un estudiante con ese nombre');
+// 	}
+// }
+
+var busqueda = prompt('A quién buscas?');
+
+function buscar(busqueda) {
+	for (i = 0; i < studentsList.length; i++) {
+		nombre = studentsList[i].firstName;
+		apellido = studentsList[i].lastName;
+
+		if (busqueda === nombre || busqueda === apellido) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+if (buscar(busqueda) === -1) {
+	console.log('No existe un alumno con ese nombre.');
+} else {
+	console.log('El alumno esta en al posicion ' + buscar(busqueda));
+}
